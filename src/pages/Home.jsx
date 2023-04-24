749916df5c721a80e17096e69bd7519d1f3b66cc
Olga <111655363+HronaOlha@users.noreply.github.com>
2023-04-25 01:39:40 +0300

    Add Loader to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
+import { Loader } from 'components/Loader/Loader';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+// import { Loader } from 'components/Loader/Loader';
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const location = useLocation();
   const [titles, setTitles] = useState([]);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(
       `https://api.themoviedb.org/3/trending/movie/day?api_key=2ead4d55a2c7da4f5313610b563685be`
     )
       .then(res => res.json())
       .then(({ results }) => {
+        setIsLoading(false);
         setTitles(results);
       });
   }, []);
@@ -18,6 +23,8 @@ const Home = () => {
   return (
     <>
       <h1>Trending today</h1>
+      {isLoading && <Loader />}
+
       {titles.length > 0 && (
         <ul>
           {titles.map(title => (
